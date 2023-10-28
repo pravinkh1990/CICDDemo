ac83130686dc6ae2ce6d591b7ed3964b44486bec
pravinkh1990 <pravinkh1990@gmail.com>
2023-10-28 23:29:01 +0530

    Sonar Setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,12 @@ plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
+
+buildscript {
+    dependencies {
+        classpath ("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:3.1.1")
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
