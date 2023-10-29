5c914a122a09affd8d9fb127e29def1c9c7412fb
pravinkh1990 <pravinkh1990@gmail.com>
2023-10-29 17:23:58 +0530

    Sonar Setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     testImplementation("org.mockito:mockito-core:3.12.4")
-// Optional if you need to detect intents.
     androidTestImplementation ("com.android.support.test.espresso:espresso-intents:2.2.1")
 }
